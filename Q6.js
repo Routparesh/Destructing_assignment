b6887a0dc5ac4d5927edc96fed2215b1eb8c947d
paresh <routpareshkumar737@gmail.com>
2023-12-05 21:03:07 +0530

    change Q6

--- Q6.js
+++ Q6.js
@@ -9,7 +9,12 @@ const person = {
 }
 
 function data(person){
-        console.log({Name: person.name, Address: person.address.city})
-}
+    const {
+        name,
+        address: { street },
+      } = person;
+      return { name, street };
+    }
+    
 
-data(person)
+console.log(data(person))
